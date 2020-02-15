b1f178d74701161faf03f82f9258a2cc1f37995b
ryutaro-kodama <slj-flz-m2@ezweb.ne.jp>
2020-02-15 18:42:08 +0900

    管理サイト作成

--- kifu_app/admin.py
+++ kifu_app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import LargeClass, MiddleClass, SmallClass, Information, Kifu
+
+admin.site.register(LargeClass)
+admin.site.register(MiddleClass)
+admin.site.register(SmallClass)
+admin.site.register(Information)
+admin.site.register(Kifu)
+
+class LargeclassAdmin(admin.ModelAdmin):
+    list_display = ('name')

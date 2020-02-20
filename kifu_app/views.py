bf47c1d6229656718bcfd82f3d753d2bb5e9bf2c
ryutaro-kodama <slj-flz-m2@ezweb.ne.jp>
2020-02-20 23:37:05 +0900

    indexページの作成

--- kifu_app/views.py
+++ kifu_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+import datetime
+
+def index(request):
+    today = datetime.date.today()
+    return render(request, 'index.html', {'today': today})

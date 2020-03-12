a274fc1ee6df06dfa5024830f5e19591b42b7803
ryutaro-kodama <slj-flz-m2@ezweb.ne.jp>
2020-03-12 18:03:25 +0900

    不要な記述の削除・ファイル名の簡略化

--- history/admin.py
+++ history/admin.py
@@ -2,13 +2,13 @@ from django.contrib import admin
 
 # Register your models here.
 
-# from .models import LargeClass, MiddleClass, SmallClass, Information, Kifu
+from .models import LargeClass, MiddleClass, SmallClass, Information, Kifu
 
-# admin.site.register(LargeClass)
-# admin.site.register(MiddleClass)
-# admin.site.register(SmallClass)
-# admin.site.register(Information)
-# admin.site.register(Kifu)
+admin.site.register(LargeClass)
+admin.site.register(MiddleClass)
+admin.site.register(SmallClass)
+admin.site.register(Information)
+admin.site.register(Kifu)
 
-# class LargeclassAdmin(admin.ModelAdmin):
-#     list_display = ('name')
+class LargeclassAdmin(admin.ModelAdmin):
+    list_display = ('name')

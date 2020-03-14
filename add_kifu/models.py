9cdfba6da982d78da070c630f186a07e5ff74470
ryutaro-kodama <slj-flz-m2@ezweb.ne.jp>
2020-03-14 14:26:09 +0900

    対局履歴を保存するテーブルの作成

--- add_kifu/models.py
+++ add_kifu/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+
+class HistoryList(models.Model):
+    game_id = models.CharField(max_length=10)
+    my_result = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(3)])
+    #自分勝ち:0, 相手勝ち:1, 引き分け:2
+    save_limit = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.game_id

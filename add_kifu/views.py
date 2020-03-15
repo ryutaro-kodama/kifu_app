bab3b9bee608f38d94c61f107aefd79c524529ed
ryutaro-kodama <slj-flz-m2@ezweb.ne.jp>
2020-03-15 18:44:18 +0900

    同期ボタンによる、新規対局データの取得

--- add_kifu/views.py
+++ add_kifu/views.py
@@ -15,9 +15,50 @@ class HistoryListView(ListView):
 
 def sync(request):
     print("OK")
-    game_ids = __getHistoryList()
+    new_game_data = __getHistoryList()
+
     return redirect('add_kifu:historyList')
 
 def __getHistoryList():
-    pass
+    # DB内の最新のゲームIDの取得
+    try:
+        last_game_id = HistoryList.objects.order_by("id").last()["game_id"]
+    except:
+        last_game_id = ''
+        # last_game_id = 'ryu914-YMTAROO-20200314_113531'
+
+    ghl = GetHistoryList()
+    ghl.viewPage(ghl.makeHistoryListURL())
+    ghl.setCookies(ghl.makeCookies())
+    ghl.refresh()
+    try:
+        ghl.waitUntilId('history_content', 10)
+    except MyNotDisplayException as e:
+        print(e)
+        return []
+
+    new_game_data = []
+    start = 1
+    while True:
+        try:
+            ghl.waitUntilText('paginate_summary', f'{start}~', 10)
+        except MyNotDisplayException as e:
+            print(e)
+            return []
+
+        temp_game_data = ghl.getGameIds()
+        for game_data in temp_game_data:
+            if last_game_id == game_data[0]:
+                break
+            new_game_data.append(game_data)
+        else:
+            if ghl.hasNextPage():
+                start += 10
+                ghl.nextPage()
+                continue
+        break
+    ghl.close()
+    ghl.quit()
+
+    return new_game_data
 

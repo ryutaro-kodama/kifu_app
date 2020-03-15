ddeed5cd0e5507b3fb97ae58204e612bfa4b2b5f
ryutaro-kodama <slj-flz-m2@ezweb.ne.jp>
2020-03-15 18:20:57 +0900

    対局履歴リストページの作成

--- add_kifu/urls.py
+++ add_kifu/urls.py
@@ -5,4 +5,6 @@ from . import views
 app_name = 'add_kifu'
 
 urlpatterns = [
+    path('historyList/', views.HistoryListView.as_view(), name='historyList'),
+    path('sync/', views.sync, name='sync'),
 ]
--- add_kifu/views.py
+++ add_kifu/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import HistoryList
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from selenium.common.exceptions import TimeoutException
+
+from .lib.historyList.getHistoryList import GetHistoryList
+from .lib.myNotDisplayException import MyNotDisplayException
+
+class HistoryListView(ListView):
+    template_name = 'historyList.html'
+    model = HistoryList
+    paginate_by = 10
+    # queryset = Information.objects.order_by('date')     # 今は静的にクエリを作成なのでこっちのやり方
+
+def sync(request):
+    print("OK")
+    game_ids = __getHistoryList()
+    return redirect('add_kifu:historyList')
+
+def __getHistoryList():
+    pass
+

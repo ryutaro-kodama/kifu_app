52c3d8775818084cea309b6c6024b8e1a0128a9c
ryutaro-kodama <slj-flz-m2@ezweb.ne.jp>
2020-03-19 18:09:38 +0900

    新規対局履歴IDのDBへの保存

--- add_kifu/views.py
+++ add_kifu/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect
-from .models import HistoryList
 from django.views.generic import ListView, DetailView
+from django.utils.timezone import make_aware
+from .models import HistoryList
 
 from selenium.common.exceptions import TimeoutException
+import datetime as dt
+import re
 
 from .lib.historyList.getHistoryList import GetHistoryList
 from .lib.myNotDisplayException import MyNotDisplayException
@@ -11,12 +14,24 @@ class HistoryListView(ListView):
     template_name = 'historyList.html'
     model = HistoryList
     paginate_by = 10
-    # queryset = Information.objects.order_by('date')     # 今は静的にクエリを作成なのでこっちのやり方
+    queryset = HistoryList.objects.order_by('id')
 
 def sync(request):
     print("OK")
+    # new_game_data = [["ryu914-sho123-20200319_174123", 1]]
     new_game_data = __getHistoryList()
+    if len(new_game_data) == 0:
+        # 新規の棋譜が無ければそのまま
+        return redirect('add_kifu:historyList')
 
+    history_list_insert = []
+    for each_new_game_data in new_game_data:
+        # each_new_game_data.append(__get30DaysLater(each_new_game_data[0]))
+        history_list_insert.append( HistoryList(game_id=each_new_game_data[0],
+                                                my_result=each_new_game_data[1],
+                                                save_limit=__get30DaysLater(each_new_game_data[0])) )
+
+    HistoryList.objects.bulk_create(history_list_insert)
     return redirect('add_kifu:historyList')
 
 def __getHistoryList():
@@ -60,5 +75,18 @@ def __getHistoryList():
     ghl.close()
     ghl.quit()
 
+    # new_game_data = [[id, 勝敗],[...]]
     return new_game_data
+
+def __get30DaysLater(game_id):
+    try:
+        datetime = re.search('[0-9]{8}_[0-9]{6}', game_id).group()
+    except AttributeError as e:
+        print(e)
+        print("対局日情報が見つかりません")
+        return "2099-12-31 23:59:59"
+    else:
+        datetime_formatted = dt.datetime.strptime(datetime, "%Y%m%d_%H%M%S")
+        save_limit = datetime_formatted + dt.timedelta(days=30)
+        return make_aware(save_limit.strftime("%Y-%m-%d %H:%M:%S"))
 

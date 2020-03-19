e8e6ef36b4b1ffe059b6f55a3b32d3228d36d537
ryutaro-kodama <slj-flz-m2@ezweb.ne.jp>
2020-03-19 22:38:04 +0900

    adminへのadd_kifuの追加

--- add_kifu/admin.py
+++ add_kifu/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import HistoryList
+
+admin.site.register(HistoryList)
+
+class LargeclassAdmin(admin.ModelAdmin):
+    list_display = ('name')

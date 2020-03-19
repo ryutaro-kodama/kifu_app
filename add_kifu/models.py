8484ef6a64124caaff89585fefe76c1d4e7c3ff1
ryutaro-kodama <slj-flz-m2@ezweb.ne.jp>
2020-03-19 18:08:48 +0900

    game_idカラムの、max_lengthの変更

--- add_kifu/models.py
+++ add_kifu/models.py
@@ -4,7 +4,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
 
 class HistoryList(models.Model):
-    game_id = models.CharField(max_length=10)
+    game_id = models.CharField(max_length=48)
     my_result = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(3)])
     #自分勝ち:0, 相手勝ち:1, 引き分け:2
     save_limit = models.DateTimeField(null=True, blank=True)

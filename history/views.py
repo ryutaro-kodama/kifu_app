26592d228e92fd53c9c2f6a55c36fd9f097fce1f
ryutaro-kodama <slj-flz-m2@ezweb.ne.jp>
2020-04-21 15:54:19 +0900

    shogitimeへのサーバー側からのデータ渡し

--- history/views.py
+++ history/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import get_object_or_404, get_list_or_404, render
 from django.views.generic import ListView, DetailView
 from .models import Information
 
+from lib.shogitime.convert2shogitime import convert2shogitime
+
 # Create your views here.
 
 class InformationListView(ListView):
@@ -13,3 +15,9 @@ class InformationListView(ListView):
 class InformationDetailView(DetailView):
     template_name = 'history/detail.html'
     model = Information
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        filename = "20200310_141940-ryu914-hiroton777.kifu"
+        context["data"] = convert2shogitime(filename)
+        return context
